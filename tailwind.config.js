9917107edd1bc9db007ab5b92e9b7b35d3e2646a
Kevin Cabon <45396141+kevincabon@users.noreply.github.com>
2024-12-04 22:51:30 +0100

    * chore(package): update vue-tsc package version to 2.1.10 * chore(package): update @volar/language-core package version to 2.1.10 * chore(package): update @volar/source-map package version to 2.4.10 * chore(package): update @volar/typescript package version to 2.4.10 * chore(package): update @vue/compiler-vue2 package version to 2.7.16 * chore(package): update @vue/language-core package version to 2.1.10 * chore(package): update muggle-string package version to 0.4.1 * chore(package): update path-browserify package version to 1.0.1 * chore(package): update vscode-uri package version to 3.0.8 * chore(package): update vue-tsc package version to 2.1.10 * chore(package): update package-lock

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import forms from '@tailwindcss/forms'
+
 export default {
   content: [
     "./index.html",
@@ -13,6 +15,6 @@ export default {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    forms,
   ],
 }
